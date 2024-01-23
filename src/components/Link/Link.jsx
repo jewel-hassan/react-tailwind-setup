8a83a21d926d9aa518913b0a76f6d6ce40363611
jewel hassan <jewelhassan399@gmail.com>
2024-01-23 17:05:56 +0600

    added new code

---
+++ src/components/Link/Link.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Link = (props) => {
+    const{id,name,path}= props.route;
+    return (
+        <li className='mr-10 hover:bg-purple-600 mt-2 font-bold'>
+             <a href={path}>{name}</a>
+        </li>
+    );
+};
+
+export default Link;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
+import Link from "../Link/Link";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
-    let routesArray = [
-        { id: 1, name: 'Home', path: '/' },
-        { id: 2, name: 'About', path: '/about' },
-        { id: 3, name: 'Services', path: '/services' },
-        { id: 4, name: 'Contact', path: '/contact' },
-        { id: 5, name: 'Blog', path: '/blog' }
-      ];
-      
-      console.log(routesArray);
-      
-    return (
-        <nav>
-            {
-                routesArray.map((route)=>{
-                    return (
-                        <div key={route.id}>
-                            <ul>
-                            <li>{route.name}</li>
-                            </ul>
-                            
-                        </div>
-                        
-                    )
-                })
-            }
-        </nav>
-    );
+  const [open, setOpen] = useState(false);
+
+  let routesArray = [
+    { id: 1, name: "Home", path: "/" },
+    { id: 2, name: "About", path: "/about" },
+    { id: 3, name: "Services", path: "/services" },
+    { id: 4, name: "Contact", path: "/contact" },
+    { id: 5, name: "Blog", path: "/blog" },
+  ];
+
+  console.log(routesArray);
+
+  return (
+    <nav className="bg-purple-400 py-2">
+      <div onClick={() => setOpen(!open)} className="md:hidden">
+        <span>
+          {open === true ? (
+            <XMarkIcon className="h-6 w-6 text-blue-800 " />
+          ) : (
+            <Bars3Icon className="h-6 w-6 text-blue-800 " />
+          )}
+        </span>
+      </div>
+
+      <ul
+        className={`md:flex absolute md:static duration-500 pl-8 pb-4 bg-purple-400 ${
+          open ? "top-6" : "-top-36"
+        }`}
+      >
+        {routesArray.map((route) => (
+          <Link key={route.id} route={route} />
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
