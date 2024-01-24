d1b0cd94d047aa6258bbebfd5569943d8851deb7
jewel hassan <jewelhassan399@gmail.com>
2024-01-24 12:13:58 +0600

    added tinybar chart

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import PriceList from "./components/Pricelist/PriceList";
 import Dashbord from "./components/Dashbord/Dashbord";
+import PhoneBar from "./components/Phonebar/PhoneBar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +18,7 @@ function App() {
         </h1>
         <PriceList />
         <Dashbord />
+        <PhoneBar/>
       </div>
     </>
   );
